564d72e8a5049785f93f519d1023a4434a864ec4
COLD <107107147+Ironman945@users.noreply.github.com>
2022-11-21 14:03:47 +0530

    Add files via upload

---
+++ teammates/index.js
@@ -0,0 +1,49 @@
+
+
+
+
+
+
+<div class="ideate">
+  <h2 class="Design">IDEATE-DESIGN-DEVELOP</h2>
+  <div class="prof">
+    <div class="dev">
+      <div class="butto1">
+        <img src="./buttonn.png" class="firstone" />
+        <h4 class="profesional"></h4>
+        <p class="development">
+          Unlock maximum individual and team potential across the Puzzle Break
+          Keystones.
+        </p>
+      </div>
+
+      <div class="butto2">
+        <img src="./buttonn.png" class="secone" />
+        <h4 class="buy"></h4>
+        <p class="back">
+          Unlock maximum individual and team potential across the Puzzle Break
+          Keystones.
+        </p>
+      </div>
+    </div>
+
+    <div class="des">
+      <div class="butto3">
+        <img src="./buttonn.png" class="thirdone" />
+        <h4 class="something"></h4>
+        <p class="for">
+          Unlock maximum individual and team potential across the Puzzle Break
+          Keystones.
+        </p>
+      </div>
+      <div class="butto4">
+        <img src="./buttonn.png" class="fourthone" />
+        <h4 class="advanced"></h4>
+        <p class="users">
+          Unlock maximum individual and team potential across the Puzzle Break
+          Keystones.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
